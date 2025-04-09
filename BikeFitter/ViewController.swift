08f95d17bbdaf64cd38450fe1dd5a70c706f7f15
timmypass17 <timmypass17@gmail.com>
2025-04-09 13:03:44 -0700

    Initial Commit

---
+++ BikeFitter/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BikeFitter
+//
+//  Created by Timmy Nguyen on 4/9/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ BikeFitterTests/BikeFitterTests.swift
@@ -0,0 +1,17 @@
+//
+//  BikeFitterTests.swift
+//  BikeFitterTests
+//
+//  Created by Timmy Nguyen on 4/9/25.
+//
+
+import Testing
+@testable import BikeFitter
+
+struct BikeFitterTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
